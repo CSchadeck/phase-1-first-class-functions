f6d0d124b09626e4ed24dd382b2bcb6108039fc0
Corey Schadeck <corey@schadeck.com>
2022-04-26 13:01:19 -0400

    commit tree c3b56ae0a136422c406f89fcbc57265999615bb0

---
+++ index.js
@@ -0,0 +1,11 @@
+function receivesAFunction(run){
+   console.log(run());
+}
+function returnsANamedFunction (){
+    return function sit(){squat}
+}
+
+function returnsAnAnonymousFunction (){
+    return function (){flip}
+
+}
